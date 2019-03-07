14604415bf8323515b9d506b8f11163cea321acc
Roman Loginov <RomzesRover@gmail.com>
2019-03-07 21:09:47 +0500

    test all necessary api methods

--- app/src/main/java/com/b2blogist/vkcommviewer/MainActivity.kt
+++ app/src/main/java/com/b2blogist/vkcommviewer/MainActivity.kt
@@ -14,11 +14,17 @@ class MainActivity : AppCompatActivity() {
 
         Thread(Runnable {
             var wms = Api.getWallMessages(-90405472L, 3, 0, "all")
-            var group = Api.getGroups(arrayListOf(90405472L), null, "cover")!![0]
+            var group = Api.getGroups(arrayListOf(90405472L), null, "cover,contacts,status,members_count,description,site")!![0]
 
             Log.v("Name", group.name)
 
             Log.v("CoverImgSrc", group.covers?.get(4)?.src ?: "no image")
+            Log.v("Contact1", group.contacts?.get(0)?.phone ?: "no contact phone")
+            Log.v("status", group.status ?: "there is no status")
+            Log.v("ava", group.photo_big ?: "there is no photo image")
+            Log.v("members_count", if (group.members_count == null) "there is no members data" else group.members_count.toString())
+            Log.v("description", group.description ?: "there is no description")
+            Log.v("site", group.site ?: "there is no site")
 
             for (one in wms) {
                 Log.v("HERE", one.text)
