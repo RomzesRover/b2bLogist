0de9b299cd13fd8c2349d150c9b1f991ce2879e6
Roman Loginov <RomzesRover@gmail.com>
2019-03-17 02:32:09 +0500

    move attachments job to utils class

--- app/src/main/java/com/b2blogist/vkcommviewer/adapters/holders/WallMessageHolder.kt
+++ app/src/main/java/com/b2blogist/vkcommviewer/adapters/holders/WallMessageHolder.kt
@@ -1,7 +1,6 @@
 package com.b2blogist.vkcommviewer.adapters.holders
 
 import android.content.Intent
-import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.text.Html
 import android.text.Spannable
@@ -14,11 +13,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.b2blogist.vkcommviewer.PostCommentsActivity
 import com.b2blogist.vkcommviewer.R
+import com.b2blogist.vkcommviewer.adapters.holders.utils.AttachmentViewJob
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.group_page_row.view.*
-import kotlinx.android.synthetic.main.simple_link.view.*
-import kotlinx.android.synthetic.main.simple_photo.view.*
-import kotlinx.android.synthetic.main.simple_video.view.*
 import vk.api.Group
 import vk.api.WallMessage
 import java.text.SimpleDateFormat
@@ -86,73 +83,19 @@ class WallMessageHolder(private val view: View, private val isOnTop: Boolean) : 
         wallMessage.attachments?.forEach {
             it.video?.let { video ->
                 val videoView = layoutInflater.inflate(R.layout.simple_video, view.attachments as ViewGroup, false)
-                Picasso.get().load(video.image_big).fit().centerCrop().into(videoView.video_image)
-                videoView.video_title.text = video.title ?: "No video name"
-                videoView.video_views.text = (video.views?.toString() ?: "No videos").plus(" views")
-                //set video click
-                videoView.video.setOnClickListener {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.vk.com/video${video.owner_id}_${video.vid}"))
-                    videoView.context.startActivity(browserIntent)
-                }
                 //add to list
-                view.attachments.addView(videoView)
+                view.attachments.addView(AttachmentViewJob.setUpVideoAttachment(videoView, video))
             }
             it.photo?.let {photo ->
                 val photoView = layoutInflater.inflate(R.layout.simple_photo, view.attachments as ViewGroup, false)
-                photoView.photo_image.visibility = View.GONE
-                photo?.photo_sizes?.let { photo_sizes ->
-                    var src: String? = ""
-                    var width = -1
-                    run breaker@{
-                        photo_sizes.forEach {photo_size ->
-                            if (photo_size.width > width) {
-                                width = photo_size.width
-                                src = photo_size.src
-                                if (width >= targetWidth)
-                                    return@breaker
-                            }
-                        }
-                    }
-                    photoView.photo_image.visibility = View.VISIBLE
-                    Picasso.get().load(src).fit().centerCrop().into(photoView.photo_image)
-                }
-                //set photo click
-                photoView.photo.setOnClickListener {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.vk.com/photo${photo.owner_id}_${photo.pid}"))
-                    photoView.context.startActivity(browserIntent)
-                }
                 //add to list
-                view.attachments.addView(photoView)
+                view.attachments.addView(AttachmentViewJob.setUpPhotoAttachment(photoView, photo, targetWidth))
             }
             it.link?.let {link ->
-                //in attachements link found show link block
+                //in attachments link found show link block
                 val linkView = layoutInflater.inflate(R.layout.simple_link, view.attachments as ViewGroup, false)
-                linkView.link_image.visibility = View.GONE
-                link.photo?.photo_sizes?.let { photo_sizes ->
-                    var src: String? = ""
-                    var width = -1
-                    run breaker@{
-                        photo_sizes.forEach {photo_size ->
-                            if (photo_size.width > width) {
-                                width = photo_size.width
-                                src = photo_size.src
-                                if (width >= targetWidth)
-                                    return@breaker
-                            }
-                        }
-                    }
-                    linkView.link_image.visibility = View.VISIBLE
-                    Picasso.get().load(src).fit().centerCrop().into(linkView.link_image)
-                }
-                linkView.link_title.text = link.title ?: "No link title"
-                linkView.link_url.text = link.url ?: "No link url"
-                //set link click
-                linkView.link.setOnClickListener {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link.url))
-                    linkView.context.startActivity(browserIntent)
-                }
                 //add to list
-                view.attachments.addView(linkView)
+                view.attachments.addView(AttachmentViewJob.setUpLinkAttachment(linkView, link, targetWidth))
             }
         }
     }
---
+++ app/src/main/java/com/b2blogist/vkcommviewer/adapters/holders/utils/AttachmentViewJob.kt
@@ -0,0 +1,80 @@
+package com.b2blogist.vkcommviewer.adapters.holders.utils
+
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.simple_link.view.*
+import kotlinx.android.synthetic.main.simple_photo.view.*
+import kotlinx.android.synthetic.main.simple_video.view.*
+import vk.api.Link
+import vk.api.Photo
+import vk.api.Video
+
+object AttachmentViewJob {
+    fun setUpVideoAttachment(videoView: View, video: Video): View{
+        Picasso.get().load(video.image_big).fit().centerCrop().into(videoView.video_image)
+        videoView.video_title.text = video.title ?: "No video name"
+        videoView.video_views.text = (video.views?.toString() ?: "No videos").plus(" views")
+        //set video click
+        videoView.video.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.vk.com/video${video.owner_id}_${video.vid}"))
+            videoView.context.startActivity(browserIntent)
+        }
+        return videoView
+    }
+
+    fun setUpLinkAttachment(linkView: View, link: Link, targetWidth: Int): View{
+        linkView.link_image.visibility = View.GONE
+        link.photo?.photo_sizes?.let { photo_sizes ->
+            var src: String? = ""
+            var width = -1
+            run breaker@{
+                photo_sizes.forEach {photo_size ->
+                    if (photo_size.width > width) {
+                        width = photo_size.width
+                        src = photo_size.src
+                        if (width >= targetWidth)
+                            return@breaker
+                    }
+                }
+            }
+            linkView.link_image.visibility = View.VISIBLE
+            Picasso.get().load(src).fit().centerCrop().into(linkView.link_image)
+        }
+        linkView.link_title.text = link.title ?: "No link title"
+        linkView.link_url.text = link.url ?: "No link url"
+        //set link click
+        linkView.link.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link.url))
+            linkView.context.startActivity(browserIntent)
+        }
+        return linkView
+    }
+
+    fun setUpPhotoAttachment(photoView: View, photo: Photo, targetWidth: Int): View{
+        photoView.photo_image.visibility = View.GONE
+        photo?.photo_sizes?.let { photo_sizes ->
+            var src: String? = ""
+            var width = -1
+            run breaker@{
+                photo_sizes.forEach {photo_size ->
+                    if (photo_size.width > width) {
+                        width = photo_size.width
+                        src = photo_size.src
+                        if (width >= targetWidth)
+                            return@breaker
+                    }
+                }
+            }
+            photoView.photo_image.visibility = View.VISIBLE
+            Picasso.get().load(src).fit().centerCrop().into(photoView.photo_image)
+        }
+        //set photo click
+        photoView.photo.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.vk.com/photo${photo.owner_id}_${photo.pid}"))
+            photoView.context.startActivity(browserIntent)
+        }
+        return photoView
+    }
+}
